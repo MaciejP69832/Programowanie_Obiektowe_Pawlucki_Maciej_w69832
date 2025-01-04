f9d35d66120f015ec26ef0f7e2d8bbde8a2a5c96
Maciej Pawłucki <w69832@student.wsiz.edu.pl>
2025-01-04 16:41:48 +0100

    Nowy Lab

--- Lab01/Lab01/obj/Debug/net8.0/Lab01.AssemblyInfo.cs
+++ Lab01/Lab01/obj/Debug/net8.0/Lab01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fea7fda777d68796d3ac5c4bed6be53c976aa2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b87176fcc7384b7904346fc77ce6b9a5145ddad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
